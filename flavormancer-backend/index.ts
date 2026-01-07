60649e8f1cb9fc5bb9afa7269f9758651773f628
ZiadTarek3000 <ziadtsalem3000@gmail.com>
2026-01-07 14:59:33 +0200

    experiment of x-frame

--- flavormancer-backend/index.ts
+++ flavormancer-backend/index.ts
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("X-Frame-Options", "SAMEORIGIN")
+  next()
+})
 
 /* ===================== Categories ===================== */
 
